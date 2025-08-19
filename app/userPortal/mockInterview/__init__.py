8ddeebe6a956f03a1e4d0d14fd257828fb639cb9
Kirti Priya <kirtipriya64@gmail.com>
2025-08-19 13:59:44 +0530

    bp name

--- app/userPortal/mockInterview/__init__.py
+++ app/userPortal/mockInterview/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-mock_interview_bp = Blueprint('mock_interview', __name__, url_prefix='/mockInterview')
+mock_interview_bp = Blueprint('mock_interview', __name__)
 
 from . import routes 
